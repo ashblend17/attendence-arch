5d21fc1a41aa8a5b63307875cc5f409cb8bb8360
abdul2801 <afdul100ahmad@gmail.com>
2023-11-07 19:28:07 +0530

    first commit

---
+++ applet.js
@@ -0,0 +1,72 @@
+const Applet = imports.ui.applet;
+const Main = imports.ui.main;
+const PopupMenu = imports.ui.popupMenu;
+const Lang = imports.lang;
+const Util = imports.misc.util;
+
+function AttendenceApplet(metadata, orientation, panel_height, instanceId) {
+  this._init(metadata, orientation, panel_height, instanceId);
+}
+
+AttendenceApplet.prototype = {
+    __proto__: Applet.TextApplet.prototype,
+  
+    _init: function(metadata, orientation, panel_height, instanceId) {
+      this._data = {};
+      Applet.TextApplet.prototype._init.call(this, orientation, panel_height, instanceId);
+      this.metadata = metadata;
+      //add icon to applet firefox
+      // this.set_applet_icon_name('firefox');
+
+      this.set_applet_label('LMS');
+
+      this._call_lms();
+      this.menu = new Applet.AppletPopupMenu(this, orientation);
+      this.menuManager = new PopupMenu.PopupMenuManager(this);
+      this.menuManager.addMenu(this.menu);
+
+
+    },
+    
+    //function to async call lms.py
+    _call_lms: function() {
+      Util.spawn_async(['python3', this.metadata.path + '/lms.py'], Lang.bind(this, function(result) {
+        this._data = JSON.parse(result);
+
+      }));
+    },
+
+
+    on_applet_clicked: function(event) {
+      // if menu open close
+      if (this.menu.isOpen) {
+        this.menu.close();
+        return;
+      }
+      this._call_lms()
+      this.menu.removeAll();
+
+      this.log('clicked');
+      // loop over the data and create a menu item for each
+      //loop over each key of the data and add key value to menu
+      for (let key in this._data) {
+        let item = new PopupMenu.PopupMenuItem(key + ': ' + this._data[key]);
+        this.menu.addMenuItem(item);
+      }
+      //show the menu
+      this.menu.open();
+
+    },
+
+    log: function(message) {
+      //using error for visibility
+      global.logError('[' + this.metadata.uuid + '] ' + message);
+
+    },
+    
+}
+
+function main(metadata, orientation, panel_height, instanceId) {
+    return new AttendenceApplet(metadata, orientation, panel_height, instanceId);
+  }
+  
---
+++ lms.py
@@ -0,0 +1,76 @@
+import requests
+from bs4 import BeautifulSoup
+from pyjsparser import parse
+import re
+import json
+import random
+login_url = 'https://lms.iiitkottayam.ac.in/login/index.php'
+# authenticated_url = 'https://lms.iiitkottayam.ac.in/some_authenticated_page'
+session = requests.Session()
+# session.cookies.set('MoodleSession','661j15q28glghusnnosq3hbnmv')
+def try_login(session):
+
+    login_response = session.get(login_url)
+    soup = BeautifulSoup(login_response.text, 'html.parser')
+    logintoken_input = soup.find('input', {'name': 'logintoken'})
+    auth_data = {
+        'logintoken' :logintoken_input.get('value'),
+        'username': '',
+        'password': '',
+    }
+
+    login_response = session.post(login_url, data=auth_data)
+    # print(session.cookies.get('MoodleSession'))
+    # write cookie to aa file
+    # with open('cookie.txt', 'w') as f:
+    #     f.write(session.cookies.get('MoodleSession'))
+try_login(session)
+
+# cookies = login_response.cookies
+# session.cookies.update(cookies)
+
+# soup = BeautifulSoup(login_response.text, 'html.parser')
+# s = soup.find_all("script")
+
+# search = re.search('"sesskey":"\w+',s[1].text)
+# key = search.group(0)[len('"sesskey":"'):]
+# print(s[1])
+# print(key)
+# https://lms.iiitkottayam.ac.in/lib/ajax/service.php?sesskey=2ygYcblMnh&info=core_course_get_enrolled_courses_by_timeline_classification
+# data = '[{"index":0,"methodname":"core_course_get_enrolled_courses_by_timeline_classification","args":{"offset":0,"limit":24,"classification":"all","sort":"fullname","customfieldname":"","customfieldvalue":""}}]'
+# x = json.loads(data)
+# print(x)
+
+# res = session.post("https://lms.iiitkottayam.ac.in/lib/ajax/service.php" , params={"sesskey" : key ,"info" :  "core_course_get_enrolled_courses_by_timeline_classification"} ,json=x)
+# print(session.cookies)
+# print(json.dumps(res.json()))
+
+
+res = session.get('https://lms.iiitkottayam.ac.in/mod/attendance/view.php?id=5891&mode=1')
+
+atten_soup = BeautifulSoup(res.text, 'html.parser')
+
+rows = atten_soup.find_all('tr')[1:-1]
+
+res = {}
+for row in rows:
+    Course = row.find(class_='colcourse cell c0').text
+    Percentage = row.find(class_='colpercentagesessionscompleted').text
+    if("ICS123" not in Course and "Calculus" not in Course and int(Course.split(' ')[1]) > 200):
+        res[Course] = Percentage
+
+
+    # print(Course.text,Percentage.text)
+
+print(json.dumps(res))
+
+
+
+# print(login_response.text)
+
+# res = session.get(see)
+# print(res.text)
+# print(authenticated_soup)
+
+session.close()
+
